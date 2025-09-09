cb6790ec3f0a2dfcaa0a1826a7170a65392af99f
YehonatanYeret <yyeret@g.jct.ac.il>
2025-09-09 22:30:14 +0300

    finish the sub2

--- sub2/ex4.py
+++ sub2/ex4.py
@@ -1,7 +1,31 @@
+import math
+
+
 def power_function(n):
     def est(x):
         return x ** n
 
+    # return the inner func
     return est
 
-power_of_2 = power_function(2)
+
+def map_of_powers(n):
+    return map(lambda x: power_function(x), range(n))
+
+
+def taylor_e(x, n=100):
+    # zip the X^n to the range, then aplay the func and divide by yhr factorial (from the range)
+    return sum(map(lambda a: a[0](x) / math.factorial(a[1]), zip(map_of_powers(n + 1), range(n + 1))))
+
+
+def main():
+    n = int(input("Enter number of powers:"))
+    result = map_of_powers(n)
+    # in the tests at the auto checker, those 2 prints are flipped (not like the example in the ex)
+    base = int(input("Enter base:"))
+    print(type(result))
+    print(tuple(map(lambda x: x(base), result)))
+
+
+if __name__ == '__main__':
+    main()
---
+++ sub2/ex5.py
@@ -0,0 +1,41 @@
+def task_manager():
+    tasks = {}
+
+    def add_task(name, state="incomplete"):
+        tasks[name] = state
+
+    def get_tasks():
+        return tasks
+
+    def complete_task(name):
+        if name in tasks:
+            tasks[name] = "complete"
+
+    return {
+        'add_task': add_task,
+        'get_tasks': get_tasks,
+        'complete_task': complete_task
+    }
+
+
+def main():
+    # Create a new task manager
+    tasks_manager = task_manager()
+    # Add tasks
+    tasks_manager['add_task']("Write email")
+    tasks_manager['add_task']("Shopping", "in progress")
+    tasks_manager['add_task']("Homework")
+    # Get the list of tasks
+    current_tasks = tasks_manager['get_tasks']()
+    print(current_tasks)
+    # Should print: {'Write email': 'incomplete', 'Shopping': 'in progress', 'Homework':'incomplete'}
+    # Mark a task as complete
+    tasks_manager['complete_task']("Write email")
+    # Get the list of tasks after marking and deleting
+    current_tasks = tasks_manager['get_tasks']()
+    print(current_tasks)
+    # Should print: {'Write email': 'complete', 'Shopping': 'in progress', 'Homework': 'incomplete'}
+
+
+if __name__ == '__main__':
+    main()
