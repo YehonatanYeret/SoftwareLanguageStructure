b52f5a2d32f1b87afcac38005be396eccbe60349
YehonatanYeret <yyeret@g.jct.ac.il>
2025-09-12 13:05:16 +0300

    finish the sub2

--- sub2/ex2.py
+++ sub2/ex2.py
@@ -7,15 +7,20 @@ l_evens = filter(lambda x: x % 2 == 0, l)
 
 # for each num append the last number (that already molted by the numbers before) mult by the
 # current number, then its return None, so we do (or a) that return the list as the reduce function expected
-cumulative_product = lambda lst, n: reduce(lambda a, x: a.append(a[-1] * x) or a, lst, [n])[1:]
-
+cumulative_product = lambda lst, n: reduce(lambda a, x: a.append(a[-1] * x) or a, list(lst)[:n], [1])[1:]
 
 # the func from ex1
 func = lambda x: x / 2 + 2
 
-# for each number add the last number (that is the sum till then) added with the number
-# by applying him the linear func
-linear_sum = lambda lst: reduce(lambda a, x: a.append(a[-1] + func(x)) or a, lst, [0])[1:]
+# for each number, add the next number then add the current number
+# by applying him the linear func. a[0] is the list that we will return and a[1] is the sum till now
+linear_sum = lambda lst: reduce(lambda a, x: (a[0] + [x + func(a[1])], a[1] + x + func(a[1])), lst, ([], 0))[0]
 
-print(f"sum of the cumulative product on the evens {sum(cumulative_product(l_evens, 1))}")
-print(f"sum of the linear sum on the odds {sum(linear_sum(l_odds))}")
+
+def main():
+    print(f"sum of the cumulative product on the evens: {sum(cumulative_product(l_evens, 100))}")
+    print(f"sum of the linear sum on the odds: {sum(linear_sum(l_odds))}")
+
+
+if __name__ == '__main__':
+    main()
