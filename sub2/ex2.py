4a8a42083aaa0b1f5199ef3bcfc55e9bbfa45c4a
Maor Noy <53058317+maor-noy@users.noreply.github.com>
2025-09-13 21:37:11 +0300

    Update ex2.py

--- sub2/ex2.py
+++ sub2/ex2.py
@@ -2,18 +2,19 @@ from functools import reduce
 
 l = list(range(1, 1001))
 
-l_odds = filter(lambda x: x % 2 != 0, l)
-l_evens = filter(lambda x: x % 2 == 0, l)
+l_odds = list(filter(lambda x: x % 2 != 0, l))
+l_evens = list(filter(lambda x: x % 2 == 0, l))
 
 # for each num append the last number (that already molted by the numbers before) mult by the
 # current number, then its return None, so we do (or a) that return the list as the reduce function expected
-cumulative_product = lambda lst, n: reduce(lambda a, x: a.append(a[-1] * x) or a, list(lst)[:n], [1])[1:]
-
+cumulative_product = lambda lst, n: list(
+    map(lambda i: reduce(lambda x, y: x * y, lst[:i+1], 1), range(n))
+)
+ 
 # the func from ex1
 func = lambda x: x / 2 + 2
 
 # for each number, add the next number then add the current number
-# by applying him the linear func. a[0] is the list that we will return and a[1] is the sum till now
 linear_sum = lambda lst: reduce(lambda a, x: (a[0] + [x + func(a[1])], a[1] + x + func(a[1])), lst, ([], 0))[0]
 
 
